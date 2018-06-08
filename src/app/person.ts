e2d3d9d90e0a8e5bdb159898feff8a07f19e0366
hazeoscar <yeganeh.ghs@gmail.com>
2018-06-08 13:05:29 -0400

    Person Model

--- src/app/person.ts
+++ src/app/person.ts
@@ -1,2 +1,21 @@
 export class Person {
+    id: number;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+
+//     getAge() : number {
+//         let today: Date = new Date();
+
+//         let age: number = today.getUTCFullYear() - this.dateOfBirth.getFullYear();
+
+//         if (this.dateOfBirth.getDate() > today.getDate()
+//             &&
+//             this.dateOfBirth.getMonth() > today.getMonth()
+//          ){
+//              age--;
+//          }
+
+//             return age;
+// }
 }
